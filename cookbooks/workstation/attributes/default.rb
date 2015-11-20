46623fdb2fdd8116a6b68c673e73a437f3198835
pwelch <paul@pwelch.net>
2015-11-20 09:47:34 -0500

    Add awscli homebrew package

--- cookbooks/workstation/attributes/default.rb
+++ cookbooks/workstation/attributes/default.rb
@@ -4,7 +4,7 @@
 #
 
 default[:workstation][:languages] = %w(go elixir erlang python rust node rebar)
-default[:workstation][:devops]    = %w(ansible packer terraform)
+default[:workstation][:devops]    = %w(awscli ansible packer terraform)
 default[:workstation][:infosec]   = %w(chkrootkit yara nmap)
 default[:workstation][:tools]     = %w(
   ack curl git the_silver_searcher wget htop-osx httpie iperf jq jrnl
