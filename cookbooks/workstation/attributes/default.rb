30260f5b3121339e36076f6d445d9e4ba31c081b
pwelch <paul@pwelch.net>
2015-12-19 19:24:34 -0500

    Add zmap to brew

--- cookbooks/workstation/attributes/default.rb
+++ cookbooks/workstation/attributes/default.rb
@@ -5,7 +5,7 @@
 
 default[:workstation][:languages] = %w(go elixir erlang python rust node rebar)
 default[:workstation][:devops]    = %w(awscli ansible packer terraform)
-default[:workstation][:infosec]   = %w(chkrootkit yara nmap)
+default[:workstation][:infosec]   = %w(chkrootkit yara nmap zmap)
 default[:workstation][:tools]     = %w(
   ack curl git the_silver_searcher wget htop-osx httpie iperf jq jrnl
   mtr openssl siege sl sqlite ssh-copy-id tmux tree watch youtube-dl
