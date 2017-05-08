a4187a9c67c35768e9c6dc2883ba9c64d502066e
Paul Welch <pwelch@users.noreply.github.com>
2017-05-08 11:01:07 -0400

    Remove Handbrake
    Removing Handbrake in favor of something else in the future. 
    
    https://objective-see.com/blog/blog_0x1D.html

--- cookbooks/workstation/attributes/default.rb
+++ cookbooks/workstation/attributes/default.rb
@@ -17,7 +17,7 @@ default[:workstation][:misc]      = %w(
 default[:workstation][:games]    = %w(vitetris nethack)
 default[:workstation][:casks]    = %w(
   adium alfred cloak dropbox gpgtools
-  grabbox handbrake iterm2 launchrocket licecap macdown
+  grabbox iterm2 launchrocket licecap macdown
   mysqlworkbench screenhero skype spotify xquartz vlc zterm
   openemu cyberduck
 )
